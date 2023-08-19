f2a8d1f24da407a84154ce61d8329dece975d0ee
Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
2023-08-19 22:50:36 +0000

    add material ui instead of tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,44 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import Button from '@mui/material/Button';
 
-function App() {
+function Copyright() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
-export default App;
+
+function BoxComponent() {
+  return (
+    <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Button>Save</Button>
+    </Box>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI Create React App example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+
+      <BoxComponent/>
+    </Container>
+  );
+}
