de1073e12f2cb0c0e13bdf2cb235081b1674de14
Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
2023-08-19 20:39:03 +0000

    add inter font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,  // or 'media' or 'class'
+
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      },
+    },
   },
+
   plugins: [],
 }
 
