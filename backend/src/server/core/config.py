e2af8d7ade20910c5b25608d77091328bad770cd
Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
2023-08-20 20:02:46 +0000

    format MONGO_URI with username and password

--- backend/src/server/core/config.py
+++ backend/src/server/core/config.py
@@ -5,7 +5,9 @@ import os
 
 MONGO_USER = config("MONGO_USER", default=None)
 MONGO_PASS = config("MONGO_PASS", default=None)
-MONGO_URI = config("MONGO_URI", default=None)
+MONGO_URI = config("MONGO_URI", default=None).format(
+    user=config.MONGO_USER, pwd=config.MONGO_PASS
+)
 
 PORT = config("FASTAPI_PORT", default=4000, cast=int)
 HOST = config("FASTAPI_HOST", default="0.0.0.0")
