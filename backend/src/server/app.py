50adc6421a53babb59d69c62492a3e4f9bca24e5
Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
2023-08-20 08:55:10 +0000

    add middleware
    this will allow react to be used as a frontend

--- backend/src/server/app.py
+++ backend/src/server/app.py
@@ -1,10 +1,26 @@
+"""FastAPI application instance."""
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from server.core import config
 
 # from .routes.route import router as RouteRouter
 
 app = FastAPI()
 
+# add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config.ORIGINS,
+    allow_credentials=config.CREDENTIALS,
+    allow_methods=config.METHODS,
+    allow_headers=config.HEADERS,
+)
+
+# add routers
 # app.include_router(ROUTER, tags=[""] prefix="")
+
 
 @app.get("/", tags=["Root"])
 async def root() -> dict:
