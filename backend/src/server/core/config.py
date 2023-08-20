bb986e2dd1d4d9655d664111c4e1a74f9db4b171
Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
2023-08-20 07:37:02 +0000

    add variables for host and debug

--- backend/src/server/core/config.py
+++ backend/src/server/core/config.py
@@ -5,4 +5,6 @@ from decouple import Config
 MONGO_USER = Config("MONGO_USER", default=None)
 MONGO_PASS = Config("MONGO_PASS", default=None)
 MONGO_URI = Config("MONGO_URI", default=None)
-FASTAPI_PORT = Config("FASTAPI_PORT", default=4000)
+PORT = Config("FASTAPI_PORT", default=4000)
+HOST = Config("FASTAPI_HOST", default="0.0.0.0")
+DEBUG = Config("DEBUG", default=False, cast=bool)
